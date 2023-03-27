0c5df3ab611b8dfd29d38ecbe677e11bf4f0c76c
kug2001 <kug2001@gmail.com>
2023-03-27 23:54:09 +0900

    feat(workflow): 워크 플로우 수정 및 시멘틱 릴리즈 워크 수정
    ISSUES CLOSED: #3

--- cz-config.js
+++ cz-config.js
@@ -1,19 +1,18 @@
 module.exports = {
   types: [
-    { value: '✨feat', name: '✨feat | 기능을 추가해요.' },
-    { value: '🐛fix', name: '🐛fix | 버그를 수정해요.' },
-    { value: '💄style', name: '💄style | 포맷팅이나 컨벤션에 따라 수정 사항을 반영해요.' },
-    { value: '🚀perf', name: '🚀perf | 성능을 개선해요.' },
-    { value: '🛠refactor', name: '🛠refactor | 코드를 리팩토링해요.' },
-    { value: '📝docs', name: '📝docs | 문서의 내용을 일부 변경해요.' },
-    { value: '✅test', name: '✅test | 테스트 코드를 추가하거나 리팩토링해요.' },
-    { value: '🚚chore', name: '🚚chore | 사소한 변경사항이나, 패키지매니저를 관리해요.' },
-    { value: '↩️ revert', name: '↩️ revert | 이전의 코드로 되돌려요.' },
-    { value: '🏂move', name: '🏂move | 디렉토리, 파일이나 코드를 새로운 위치로 이동시켜요.' },
-    { value: '🗑remove', name: '🗑remove | 쓸모없는 디렉토리, 파일이나 코드를 삭제해요.' },
-    { value: '⚙️ ci', name: '⚙️ ci | CI를 업데이트해요.' }
+    { value: 'feat', name: 'feat | 기능을 추가해요.' },
+    { value: 'fix', name: 'fix | 버그를 수정해요.' },
+    { value: 'style', name: 'style | 포맷팅이나 컨벤션에 따라 수정 사항을 반영해요.' },
+    { value: 'perf', name: 'perf | 성능을 개선해요.' },
+    { value: 'refactor', name: 'refactor | 코드를 리팩토링해요.' },
+    { value: 'docs', name: 'docs | 문서의 내용을 일부 변경해요.' },
+    { value: 'test', name: 'test | 테스트 코드를 추가하거나 리팩토링해요.' },
+    { value: 'chore', name: 'chore | 사소한 변경사항이나, 패키지매니저를 관리해요.' },
+    { value: 'revert', name: 'revert | 이전의 코드로 되돌려요.' },
+    { value: 'move', name: 'move | 디렉토리, 파일이나 코드를 새로운 위치로 이동시켜요.' },
+    { value: 'remove', name: 'remove | 쓸모없는 디렉토리, 파일이나 코드를 삭제해요.' },
+    { value: 'ci', name: 'ci | CI를 업데이트해요.' }
   ],
-  allowCustomScopes: true,
   scopes: [
     'component',
     'css-style',
@@ -21,16 +20,12 @@ module.exports = {
     'store',
     'util',
     'api',
-    'wrong codes',
-    'spaghetti codes',
-    'alien codes',
     'assets',
     'package',
     'lint',
     'formatting',
     'config',
     'workflow',
-    // NOTE: .releaserc.js.js
     'breaking',
     'no-release',
     'README'
