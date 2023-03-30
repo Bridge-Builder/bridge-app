bb6a0d2855d6b66882dbadce90b203249d4aa7a6
kug2001 <kug2001@gmail.com>
2023-03-30 19:00:12 +0900

    feat(component): ui에 대한 모듈과 모듈 페이지 추가
    ISSUES CLOSED: #14

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// 넥스트 설정
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true
---
+++ src/config/colorConfig.ts
@@ -0,0 +1,29 @@
+export enum PRIMARY_COLOR {
+  MAIN = '#4fc3f7',
+  ACCENT = '#2e73ab',
+  SUCCESS = '#388e3c',
+  ERROR = '#d32f2f'
+}
+
+export enum SECONDARY_COLOR {
+  MAIN = '#000',
+  ACCENT = '#000',
+  SUCCESS = '#388e3c',
+  ERROR = '#d32f2f'
+}
+
+export enum TEXT {
+  BLACK = '#000',
+  WHITE = '#FFF',
+  BRAND = '#67b7f7'
+}
+
+export enum GRAY {
+  OP_30 = 'rgba(0, 0, 0, 0.03)',
+  OP_50 = 'rgba(0, 0, 0, 0.05)',
+  OP_100 = 'rgba(0, 0, 0, 0.1)',
+  OP_400 = 'rgba(0, 0, 0, 0.4)',
+  OP_600 = 'rgba(0, 0, 0, 0.6)',
+  OP_700 = 'rgba(0, 0, 0, 0.7)',
+  OP_800 = 'rgba(0, 0, 0, 0.8)'
+}
